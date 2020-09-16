1eb1bca05c429a13669d9520680c4a9a796ea08a
chan <lsc4237@gmail.com>
2020-09-16 21:10:16 +0900

    KAKAO CHATBOT 송수신

--- src/main/java/com/dsu/chat/KKORestAPI.java
+++ src/main/java/com/dsu/chat/KKORestAPI.java
@@ -18,36 +18,44 @@ import java.util.Map;
 public class KKORestAPI {
 
     //카카오톡 오픈빌더로 리턴할 스킬 API
-    @RequestMapping(value = "/kkoChat/v1" , method= {RequestMethod.POST , RequestMethod.GET },headers = {"Accept=application/json"})
-    public HashMap<String,Object> callAPI(@RequestBody Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(value = "/kkoChat/v1", method = {RequestMethod.POST, RequestMethod.GET}, headers = {"Accept=application/json"})
+    public HashMap<String, Object> callAPI(@RequestBody Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
 
         HashMap<String, Object> resultJson = new HashMap<>();
 
-        try{
+        try {
 
             ObjectMapper mapper = new ObjectMapper();
             String jsonInString = mapper.writeValueAsString(params);
             System.out.println(jsonInString);
 
-            List<HashMap<String,Object>> outputs = new ArrayList<>();
-            HashMap<String,Object> template = new HashMap<>();
+            ChatData chatdata = new ChatData();
+            /* 발화 처리 부분 * */
+            HashMap<String, Object> userRequest = (HashMap<String, Object>) params.get("userRequest");
+            String utter = userRequest.get("utterance").toString().replace("\n", "");
+
+            String rtnStr = "";
+            rtnStr = chatdata.response(chatdata.request(utter));
+            /* 발화 처리 끝*/
+
+            List<HashMap<String, Object>> outputs = new ArrayList<>();
+            HashMap<String, Object> template = new HashMap<>();
             HashMap<String, Object> simpleText = new HashMap<>();
             HashMap<String, Object> text = new HashMap<>();
 
-            text.put("text","코딩32 발화리턴입니다.");
-            simpleText.put("simpleText",text);
+            text.put("text", rtnStr);
+            simpleText.put("simpleText", text);
             outputs.add(simpleText);
 
-            template.put("outputs",outputs);
+            template.put("outputs", outputs);
 
-            resultJson.put("version","2.0");
-            resultJson.put("template",template);
+            resultJson.put("version", "2.0");
+            resultJson.put("template", template);
 
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
 
         return resultJson;
     }
-
 }
