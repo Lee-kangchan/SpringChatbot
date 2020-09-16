52aa2376b7ee79de09243205db90be479c805c2f
chan <lsc4237@gmail.com>
2020-09-16 17:56:28 +0900

    response test

--- src/main/java/com/dsu/chat/KKORestAPI.java
+++ src/main/java/com/dsu/chat/KKORestAPI.java
@@ -9,23 +9,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class KKORestAPI {
+
     //카카오톡 오픈빌더로 리턴할 스킬 API
     @RequestMapping(value = "/kkoChat/v1" , method= {RequestMethod.POST , RequestMethod.GET },headers = {"Accept=application/json"})
-    public String callAPI(@RequestBody Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+    public HashMap<String,Object> callAPI(@RequestBody Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+
+        HashMap<String, Object> resultJson = new HashMap<>();
 
         try{
+
             ObjectMapper mapper = new ObjectMapper();
             String jsonInString = mapper.writeValueAsString(params);
             System.out.println(jsonInString);
-            int x = 0;
+
+            List<HashMap<String,Object>> outputs = new ArrayList<>();
+            HashMap<String,Object> template = new HashMap<>();
+            HashMap<String, Object> simpleText = new HashMap<>();
+            HashMap<String, Object> text = new HashMap<>();
+
+            text.put("text","코딩32 발화리턴입니다.");
+            simpleText.put("simpleText",text);
+            outputs.add(simpleText);
+
+            template.put("outputs",outputs);
+
+            resultJson.put("version","2.0");
+            resultJson.put("template",template);
+
         }catch (Exception e){
 
         }
-        return "index";
+
+        return resultJson;
     }
 
 }
