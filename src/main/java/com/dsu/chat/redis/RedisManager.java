2b83bd22915328d7a82624e9de1d66fedad262ef
chan <lsc4237@gmail.com>
2020-09-22 22:32:42 +0900

    redis set map 구조 추가

--- src/main/java/com/dsu/chat/redis/RedisManager.java
+++ src/main/java/com/dsu/chat/redis/RedisManager.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Service
@@ -12,16 +15,52 @@ public class RedisManager {
     @Resource(name = "jedis")
     Jedis jedis;
 
-    public void setData(String name){
-        jedis.sadd("data",name);
+    public void admin(){ // 관리자 세션 (50분동안 움직이지 않으면 챗봇에 대한 데이터를 받을 수 없음)
+        jedis.set("admin", "1");
+        jedis.expire("admin",3000);
     }
-    public Set<String> getData(){
-        return jedis.smembers("data");
+
+    // 질문을 받는 공간
+    public void setData(String key , String name){
+        jedis.sadd(key,name);
+    }
+    // 질문의 목록
+    public Set<String> getData(String key){
+        return jedis.smembers(key);
+    }
+    //질문 처리 했으면 삭제
+    public void delData(String name){
+        jedis.srem(name);
     }
 
 
-    public String test(){
-        return jedis.get("hello");
+
+    //response data
+    public String getResponse (String data) {
+        return jedis.hget("response" , data);
+    }
+    //response data insert
+    public void setResponse(String key, String value){
+        jedis.hset("response" , key , value);
+    }
+
+
+    //Button Data 있으면 출력 없으면 null
+    public Map<String,String> buttonData(String data){
+        Map<String,String> hash = jedis.hgetAll("button");
+        if(!hash.get(data).isEmpty()){
+            return hash;
+        }
+        return null;
+    }
+    // button이 필요한 데이터 추가
+    public void setButton(String data, String label, String url){
+        Map<String, String> hash =  new HashMap<>();
+        jedis.hset(data , "label" , label);
+        jedis.hset(data , "action", "webLink");
+        jedis.hset(data , "webLinkUrl",url);
 
     }
+
+
 }
