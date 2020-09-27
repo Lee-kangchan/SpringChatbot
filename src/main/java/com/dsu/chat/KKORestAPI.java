2b1405cf33aa83d787f5d571db115635ce38b4f7
chan <lsc4237@gmail.com>
2020-09-27 22:29:17 +0900

    문제 수정

--- src/main/java/com/dsu/chat/KKORestAPI.java
+++ src/main/java/com/dsu/chat/KKORestAPI.java
@@ -138,6 +138,7 @@ public class KKORestAPI {
 
     @DeleteMapping(value = "/question") // 질문 삭제
     public void delQuetion(@RequestBody String question){
+        question = question.replace(" " , "").replace("\n","");
         logger.info("질문의 답은 ? "+question);
         managers.delData("question",question);
     }
